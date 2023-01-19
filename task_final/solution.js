54e921e191fd7ab7014cf9bfffae1d896748f03f
Мария Перова <m.perova@tradedealer.ru>
2023-01-19 13:01:55 +0500

    task-final

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,20 +34,18 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
+    let result = {data: {client: '', order: {}, goods: []}};
+
+    result.data.client = `${name} ${phone}`;
+    result.data.order.address = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв. ${address.flat}`;
+    result.data.order.sum = sum;
 
     let countOfGoods = goods.length;
-
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+    for (let i = 0; i < countOfGoods; i += 1) {
+        let good = {title: goods[i].title, count: goods[i].count};
+        result.data.goods.push(good);
     }
-
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
-
-    data.client = 'Иван';
-
-    let jsonData = JSON.stringify(data);
-
+    let jsonData = JSON.stringify(result);
+    
     return jsonData;
 }
